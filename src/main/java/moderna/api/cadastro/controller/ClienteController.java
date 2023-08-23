a59d73ad1fceade61ed878a82335b96bcf1ba3e1
miquiles <miquiles.lima@hotmail.com>
2023-08-23 10:56:25 -0300

    conf basica

---
+++ src/main/java/moderna/api/cadastro/controller/ClienteController.java
@@ -0,0 +1,9 @@
+package moderna.api.cadastro.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+}
---
+++ src/main/java/moderna/api/cadastro/entidades/Cliente.java
@@ -0,0 +1,59 @@
+package moderna.api.cadastro.entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "api-cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String razaoSocial;
+    @Column(nullable = false, length = 15) //not null
+    private String cnpj;
+    private String nomeSocio;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNomeSocio() {
+        return nomeSocio;
+    }
+
+    public void setNomeSocio(String nomeSocio) {
+        this.nomeSocio = nomeSocio;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", razaoSocial='" + razaoSocial + '\'' +
+                ", cnpj='" + cnpj + '\'' +
+                ", nomeSocio='" + nomeSocio + '\'' +
+                '}';
+    }
+}
