ff7295b4a32613d60f316b9e05d03f1136a28c1f
miquiles <miquiles.lima@hotmail.com>
2023-08-30 15:19:07 -0300

    simples get e post

--- src/main/java/moderna/api/cadastro/controller/ClienteController.java
+++ src/main/java/moderna/api/cadastro/controller/ClienteController.java
@@ -1,9 +1,30 @@
 package moderna.api.cadastro.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import moderna.api.cadastro.entidades.Cliente;
+import moderna.api.cadastro.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
+
+    @Autowired //anotação para injetar uma dependencia.
+    ClienteRepository clienteRepository;
+
+    @PostMapping("/salvar")
+    public void salvarCliente(@RequestBody Cliente cliente){
+        clienteRepository.save(cliente);
+    }
+
+    @GetMapping("/buscar/{id}")
+    public Optional<Cliente> buscarPorId(@PathVariable Long id){
+        return clienteRepository.findById(id);
+    }
+
+
 }
